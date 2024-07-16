a825cf6f4cd071728f479552e058d1162100e65c
Robert Douglass <rob@robshouse.net>
2024-07-16 20:40:43 +0200

    Mount for SQLite

--- 01_install_django_sqlite_upsun/myproject/myproject/settings.py
+++ 01_install_django_sqlite_upsun/myproject/myproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +75,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Set the DB_DIR to one level higher than BASE_DIR
+DB_DIR = BASE_DIR.parent
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db/db.sqlite3',
+        'NAME': os.path.join(DB_DIR, 'db', 'db.sqlite3'),
     }
 }
 
@@ -121,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from .settings_psh import *
